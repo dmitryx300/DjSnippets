4e9c7eb7eb18d91a4fcbe88d179b21bce80e6453
Vadim <vadimx30@gmail.com>
2023-03-17 15:10:09 +0300

    Add admin site

--- MainApp/models.py
+++ MainApp/models.py
@@ -8,10 +8,16 @@ LANGS = (
     ('cpp', 'C++')
 )
 
+
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
-    public = models.BooleanField(default=True) # True - public, False - private
+    user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, blank=True, null=True)
+    # True - public, False - private
+    public = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}{self.name, self.lang}'
