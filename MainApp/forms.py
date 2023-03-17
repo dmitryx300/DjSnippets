c458f124ea29dfd119b0cd23c325252117a00887
Vadim <vadimx30@gmail.com>
2023-03-17 13:12:48 +0300

    Add public/private snippet flag. Module6 practice task2

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -3,15 +3,15 @@ from MainApp.models import Snippet
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
-       labels = {'name':'', 'lang': '', 'code': ''}
-       widgets = {
-           'name': TextInput(attrs={'placeholder': 'Название сниппета'}),
-           'code': Textarea(attrs={'placeholder': 'Код сниппета'})
-       }
-       # С помощью exclude можно указать поля, которые нужно исключить
-       # Вместе fields и exclude использовать нельзя
-       # exclude = ['lang'] 
+    class Meta:
+        model = Snippet
+        # Описываем поля, которые будем заполнять в форме
+        fields = ['name', 'lang', 'code', 'public']
+        labels = {'name': '', 'lang': '', 'code': '', 'public': ''}
+        widgets = {
+            'name': TextInput(attrs={'placeholder': 'Название сниппета'}),
+            'code': Textarea(attrs={'placeholder': 'Код сниппета'})
+        }
+        # С помощью exclude можно указать поля, которые нужно исключить
+        # Вместе fields и exclude использовать нельзя
+        # exclude = ['lang']
